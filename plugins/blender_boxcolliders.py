636aa77087ce31dd4df9c3aa7e89484a6ba2f23b
iyenal <iyenal222@hotmail.fr>
2023-04-01 01:48:37 +0200

    Pushed correct version

--- plugins/blender_boxcolliders.py
+++ plugins/blender_boxcolliders.py
@@ -1,10 +1,15 @@
+# Gen7 Engine's plugin for automatic exportation of collision cubes (in "Colliders" collection) to Gen7
+# Licence: Public Domain (CC Zero)
+
 import bpy
+
+# generate camera: position accurate, FOV 80deg, 
 
 collection = bpy.data.collections["Colliders"]
 
 print("Exporting box colliders...")
 
-collisionScript = 'createAssetCube("cube")\n'
+collisionScript = 'createAssetCube("cube");\n'
 
 for obj in collection.all_objects:
     print(obj.name)
@@ -19,16 +24,13 @@ for obj in collection.all_objects:
     sz = str(round(obj.scale.z,4))
     
     print(px+","+py+","+pz+","+sx+","+sy+","+sz)
-    collisionScript+='createObjectwithScale("'+str(obj.name)+'", cube,'+px+","+py+","+pz+","+sx+","+sy+","+sz+")\n"
-    collisionScript+='addToCollision("'+str(obj.name)+'")\n'
+    collisionScript+='createObjectwithScale("'+str(obj.name)+'", "cube",'+px+","+py+","+pz+","+sx+","+sy+","+sz+");\n"
+    collisionScript+='addToCollision("'+str(obj.name)+'");\n'
     
 print("\nUse this script to generate colliders for your scene:\n")
 
 print(collisionScript)
 bpy.context.window_manager.clipboard = collisionScript
 
-createAssetCube("cube")
-createObjectwithScale("Cube.004", cube,2.8671,2.2938,0.0,1.0,1.0,1.0)
-addToCollision("Cube.004")
-createObjectwithScale("Cube.003", cube,4.6241,-3.8397,0.0,2.1784,0.4529,1.0)
-addToCollision("Cube.003")
+print("\nScript pasted in clipboard too.\n")
+print("Don't forget to set your player collider with setCollisionController")
